74fc6380493bc442c2700b3cf63ba308513dbf7f
ryandadeng <ryandadeng@gmail.com>
2018-11-25 13:00:06 +1100

    UPDATED

--- resources/views/googlerecaptchav3/field.blade.php
+++ resources/views/googlerecaptchav3/field.blade.php
@@ -1,5 +1,4 @@
 <input type="hidden" id="{{$id}}" name="{{$name}}">
-
 <script>
     grecaptcha.ready(function () {
         grecaptcha.execute('{{$publicKey}}', {action: '{{$action}}'}).then(function (token) {
