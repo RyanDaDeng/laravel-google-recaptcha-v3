efef647cecdd7a3f46f9b54b7b2e8eb59b2440e9
ryandadeng <ryandadeng@gmail.com>
2018-12-25 18:51:40 +1100

    updated ci/cd

---
+++ tests/RequestTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace TimeHunter\Tests;
+
+use PHPUnit\Framework\TestCase;
+use TimeHunter\LaravelGoogleCaptchaV3\Core\CurlRequestClient;
+use TimeHunter\LaravelGoogleCaptchaV3\Core\GoogleReCaptchaV3Response;
+use TimeHunter\LaravelGoogleCaptchaV3\Core\GuzzleRequestClient;
+use TimeHunter\LaravelGoogleCaptchaV3\GoogleReCaptchaV3;
+use TimeHunter\LaravelGoogleCaptchaV3\Configurations\ReCaptchaConfigV3;
+
+
+class RequestTest extends TestCase
+{
+
+
+    public function testCurlRequest()
+    {
+
+        $client = new CurlRequestClient();
+
+        $response = $client->post('https://www.google.com/recaptcha/api/siteverify', [
+            'secret' => 'test',
+            'remoteip' => null,
+            'response' => 'test',
+        ]);
+
+        $response = new GoogleReCaptchaV3Response(json_decode($response,1), null, '');
+        $this->assertEquals(false, $response->isSuccess());
+        $this->assertEquals(2, count($response->getErrorCodes()));
+
+    }
+
+
+
+    public function testGuzzleRequest()
+    {
+
+        $client = new GuzzleRequestClient();
+
+        $response = $client->post('https://www.google.com/recaptcha/api/siteverify', [
+            'secret' => 'test',
+            'remoteip' => null,
+            'response' => 'test',
+        ]);
+
+        $response = new GoogleReCaptchaV3Response(json_decode($response,1), null, '');
+        $this->assertEquals(false, $response->toArray()['success']);
+        $this->assertEquals(2, count($response->getErrorCodes()));
+
+    }
+
+
+}
