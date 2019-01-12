ed0258ac88208630cea95070bb9f5e756af02c3f
ryandadeng <ryandadeng@gmail.com>
2019-01-12 13:13:04 +1100

    ADDED TEST CASES for setAction

--- tests/ScoreTest.php
+++ tests/ScoreTest.php
@@ -318,4 +318,40 @@ class ScoreTest extends TestCase
         $response = $service->setScore(0.8)->verifyResponse('test response');
         $this->assertEquals(false, $response->isSuccess());
     }
+
+
+
+    public function testScore12()
+    {
+        // Create a stub for the SomeClass class.
+        $configStub = $this->createMock(ReCaptchaConfigV3::class);
+
+        // Configure the stub.
+        $configStub->method('isServiceEnabled')
+            ->willReturn(true);
+
+        $configStub->method('isScoreEnabled')
+            ->willReturn(true);
+
+        $configStub->method('getSetting')
+            ->willReturn([
+                [
+                    'action' => 'contact_us',
+                    'threshold' => 0.6,
+                    'score_comparision' => true,
+                ],
+            ]);
+
+        $testJson = '{ "success": true, "challenge_ts": "2018-12-25T03:35:32Z", "hostname": "ryandeng.test", "score": 0.7, "action": "contact_us" }';
+
+        $clientStub = $this->createMock(GuzzleRequestClient::class);
+        $clientStub->method('post')
+            ->willReturn($testJson);
+
+        $_service = new GoogleReCaptchaV3Service($configStub, $clientStub);
+        $service = new GoogleReCaptchaV3($_service);
+
+        $response = $service->setScore(0.8)->verifyResponse('test response');
+        $this->assertEquals(false, $response->isSuccess());
+    }
 }
