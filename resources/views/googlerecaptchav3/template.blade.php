3c3d2de682372b11e9c64b4daec304661a6aba2b
ryandadeng <ryandadeng@gmail.com>
2019-01-26 17:11:44 +1100

    added check id

--- resources/views/googlerecaptchav3/template.blade.php
+++ resources/views/googlerecaptchav3/template.blade.php
@@ -23,17 +23,19 @@
         function onloadCallback() {
                     @foreach($mappers as $action=>$fields)
                     @foreach($fields as $field)
-            let client{{$field}} = grecaptcha.render('{{$field}}', {
-                    'sitekey': '{{$publicKey}}',
-                        @if($inline===true) 'badge': 'inline', @endif
-                    'size': 'invisible',
-                    'hl': '{{$language}}'
-                });
-            grecaptcha.ready(function () {
-                grecaptcha.execute(client{{$field}}, {
-                    action: '{{$action}}'
-                });
-            });
+                    if (document.getElementById('{{$field}}')) {
+                        let client{{$field}} = grecaptcha.render('{{$field}}', {
+                            'sitekey': '{{$publicKey}}',
+                            @if($inline===true) 'badge': 'inline', @endif
+                            'size': 'invisible',
+                            'hl': '{{$language}}'
+                        });
+                        grecaptcha.ready(function () {
+                            grecaptcha.execute(client{{$field}}, {
+                                action: '{{$action}}'
+                            });
+                        });
+                    }
             @endforeach
             @endforeach
         }
