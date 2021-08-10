3b4963381e3ec9978e778bb90225648a29fe9f8c
Luis Rossi <53585086+luisrossi@users.noreply.github.com>
2021-08-10 11:05:06 -0300

    add pt_BR lang

---
+++ resources/lang/pt-BR/messages.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'ERROR_MISSING_INPUT' => 'Solicitação de entrada não existente.',
+    'ERROR_UNABLE_TO_VERIFY' => 'Impossível verificar.',
+    'ERROR_HOSTNAME' => 'Nome de Host não correspondente.',
+    'ERROR_ACTION' => 'Ação não correspondente.',
+    'ERROR_SCORE_THRESHOLD' => 'Pontuação inferior ao mínimo permitido.',
+    'ERROR_TIMEOUT' => 'Tempo limite excedido',
+    'SUCCESS' => 'Verificação aprovada com sucesso.',
+    'TIMEOUT_OR_DUPLICATE' => 'Tempo limite excedido ou requisição duplicada',
+];
